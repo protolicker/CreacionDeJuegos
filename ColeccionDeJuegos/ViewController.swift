3dc9c0f1503016b653ce6a85dcc3fdc2dd85946b
Renato Usnayo <>
2020-05-28 20:23:23 -0700

    primero comit para

---
+++ ColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by Renato Usnayo on 5/28/20.
+//  Copyright © 2020 Renato Usnayo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
